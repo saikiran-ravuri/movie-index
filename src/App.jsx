9c8a46be0f4d87d458cf4d912788e7ce63d0703a
Ravuri Sai Kiran <saikiranravuri786@gmail.com>
2026-07-27 23:55:53 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Start CineVault</h1>
+      <p>Your project journey begins...</p>
+    </main>
+  );
+}
+
+export default App;
